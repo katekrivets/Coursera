b188f16f8beb135687e3ce5beab577608c6abe23
Kate <katekrivets@hotmail.com>
2017-09-18 21:12:39 +0300

    module4 done

---
+++ module4/script.js
@@ -0,0 +1,42 @@
+/*
+
+Hello Yaakov
+
+Good Bye John
+
+Good Bye Jen
+
+Good Bye Jason
+
+Hello Paul
+
+Hello Frank
+
+Hello Larry
+
+Hello Paula
+
+Hello Laura
+
+Good Bye Jim
+
+*/ 
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (i = 0; i < names.length; i++) {
+   var firstLetter = names[i].charAt(0).toLowerCase(0);
+    if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+    }
+
+  else {
+
+    helloSpeaker.speak(names[i]);
+  }
+
+}
+
+})();
